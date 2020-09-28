5653cc8d57df3f51a7b19b4c4ca6a2455895688d
Anurag Raina <rainakalhan90@gmail.com>
2020-09-28 16:53:11 +0530

    Modify template for first use.

--- components/Header.js
+++ components/Header.js
@@ -17,10 +17,10 @@ const Header = (props) => (
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+                <li><a onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
+                <li><a onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
+                <li><a onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                <li><a onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
             </ul>
         </nav>
     </header>
